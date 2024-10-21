432c8cc09389eb0df7bbbb0eefb12e6c24b6e5e5
Mahesh Singh Madai <maheshmadai977@gmail.com>
2024-10-21 17:57:54 +0545

    apiUrl modify for local development

--- clientApp/src/environments/environment.development.ts
+++ clientApp/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7201/',
+    apiUrl: 'https://localhost:7201/api/',
     stripePublicKey : 'pk_test_51QASthD1OgzzJzevS4okyoWONnf8CFw7Zd8dhiUGlTbNIuYiYubqfcBaDZyHiQTMUftFqv2Scv3Ox5MZXSgtBpBW001yR6KR9V'
 };
