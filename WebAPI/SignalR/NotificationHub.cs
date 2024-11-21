214f780ff2a9685dc462931487238c5b2bab485d
Mahesh Singh Madai <maheshmadai977@gmail.com>
2024-11-21 10:18:51 +0545

    spacing managed

--- WebAPI/SignalR/NotificationHub.cs
+++ WebAPI/SignalR/NotificationHub.cs
@@ -12,23 +12,18 @@ namespace WebAPI.SignalR
         public override Task OnConnectedAsync()
         {
             var email = Context.User.GetEmail();
-
             if (!string.IsNullOrEmpty(email)) UserConnections[email] = Context.ConnectionId;
-
             return base.OnConnectedAsync();
         }
         public override Task OnDisconnectedAsync(Exception? exception)
         {
             var email = Context.User.GetEmail();
-
             if (!string.IsNullOrEmpty(email)) UserConnections.TryRemove(email, out _);
-
             return base.OnDisconnectedAsync(exception);
         }
         public static string? GetConnectionIdByEmail(string email)
         {
             UserConnections.TryGetValue(email, out var connectionId);
-
             return connectionId;
         }
     }
